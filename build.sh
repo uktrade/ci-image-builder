1093095caea3cdc260ca45f4e125a440cbf0260a
Jayesh Patel <jayesh.patel@digital.trade.gov.uk>
2023-05-24 11:28:11 +0100

    fix branch missing bug

--- build.sh
+++ build.sh
@@ -14,11 +14,17 @@ LOG_LEVEL="DEBUG"
 #ACCOUNT_NAME=$(aws iam list-account-aliases |jq -r ".[][]")
 GIT_TAG=$(git describe --tags --abbrev=0)
 GIT_COMMIT=$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION |cut -c1-7)
+
+if [ -z $CODEBUILD_WEBHOOK_TRIGGER ]; then
+  GIT_BRANCH=$(git branch --show-current)
+else
+  GIT_BRANCH=$(echo $CODEBUILD_WEBHOOK_TRIGGER | awk -F "/" '{print $2}')
+fi
 #GIT_BRANCH=$(git branch --show-current)
-GIT_BRANCH=$(echo $CODEBUILD_WEBHOOK_TRIGGER | awk -F "/" '{print $2}')
+#GIT_BRANCH=$(echo $CODEBUILD_WEBHOOK_TRIGGER | awk -F "/" '{print $2}')
 #APP_NAME=$(echo $CODEBUILD_SRC_DIR |awk -F / '{print $(NF)}')
 
-if [ codebuild/process.yml ];then
+if [ -f "codebuild/process.yml" ];then
   BUILDSPEC_PATH="codebuild/process.yml"
 else
   BUILDSPEC_PATH="copilot/process.yml"
