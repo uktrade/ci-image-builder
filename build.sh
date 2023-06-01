1d00e81f21d1fa66e3eeb47ae0d57362ea767442
Yusuf <yusuf@madetech.com>
2023-06-01 13:42:37 +0100

    Update `build` script to prevent globbing/word splitting

--- build.sh
+++ build.sh
@@ -15,12 +15,12 @@ LIFECYCLE="${ECR_PATH}/buildpacksio/lifecycle:${LIFECYCLE_VERSION}"
 DOCKERREG=$(aws sts get-caller-identity --query Account --output text).dkr.ecr.eu-west-2.amazonaws.com
 LOG_LEVEL="DEBUG"
 GIT_TAG=$(git describe --tags --abbrev=0)
-GIT_COMMIT=$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION |cut -c1-7)
+GIT_COMMIT=$(echo "$CODEBUILD_RESOLVED_SOURCE_VERSION" | cut -c1-7)
 
-if [ -z $CODEBUILD_WEBHOOK_TRIGGER ]; then
+if [ -z "$CODEBUILD_WEBHOOK_TRIGGER" ]; then
   GIT_BRANCH=$(git branch --show-current)
 else
-  GIT_BRANCH=$(echo $CODEBUILD_WEBHOOK_TRIGGER | awk -F "/" '{print $2}')
+  GIT_BRANCH=$(echo "$CODEBUILD_WEBHOOK_TRIGGER" | awk -F "/" '{print $2}')
 fi
 
 if [ -f "codebuild/process.yml" ];then
@@ -43,7 +43,7 @@ else
 fi
 
 if [ -f "runtime.txt" ]; then
-  PYTHON_VERSION="--env BP_CPYTHON_VERSION=$(cat runtime.txt |awk -F - '{print $2}')"
+  PYTHON_VERSION="--env BP_CPYTHON_VERSION=$(awk -F - '{print $2}' < runtime.txt)"
 else
   PYTHON_VERSION=""
 fi
@@ -59,7 +59,7 @@ docker tag ${LIFECYCLE} buildpacksio/lifecycle:${LIFECYCLE_VERSION}
 
 docker images
 
-aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin ${DOCKERREG}
+aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin "${DOCKERREG}"
 
 cp Procfile Procfile_tmp
 
@@ -67,36 +67,35 @@ APP_NAME=$(niet ".application.name" ${BUILDSPEC_PATH})
 
 count=1
 
-# If there are multiple Procfiles loop through them and create multiple OCI images for each instance
+# If there are multiple processes, loop through them and create multiple OCI images for each instance
 for PROC in $(niet ".application.process" ${BUILDSPEC_PATH})
 do
   sed -n "$count p" Procfile_tmp > Procfile
 
   # If ECR repo does not exist create it
-  aws ecr describe-repositories --repository-names ${APP_NAME}/${PROC} --region eu-west-2 >/dev/null
+  aws ecr describe-repositories --repository-names "${APP_NAME}"/"${PROC}" --region eu-west-2 >/dev/null
   status=$?
-  [ $status -ne 0 ] && aws ecr create-repository --repository-name ${APP_NAME}/${PROC} --region eu-west-2
+  [ $status -ne 0 ] && aws ecr create-repository --repository-name "${APP_NAME}"/"${PROC}" --region eu-west-2
 
   # Build image and push to ECR
-  pack build ${DOCKERREG}/${APP_NAME}/${PROC} \
-    --tag ${DOCKERREG}/${APP_NAME}/${PROC}:${GIT_TAG} \
-    --tag ${DOCKERREG}/${APP_NAME}/${PROC}:${GIT_COMMIT} \
-    --tag ${DOCKERREG}/${APP_NAME}/${PROC}:${GIT_BRANCH} \
+  IMAGE="${DOCKERREG}"/"${APP_NAME}"/"${PROC}"
+  pack build "$IMAGE" \
+    --tag "$IMAGE":"${GIT_TAG}" \
+    --tag "$IMAGE":"${GIT_COMMIT}" \
+    --tag "$IMAGE":"${GIT_BRANCH}" \
     --builder ${INPUT_BUILDER} \
-    ${BUILDPACKS} \
+    "${BUILDPACKS}" \
     --env BP_LOG_LEVEL=${LOG_LEVEL} \
-    ${PYTHON_VERSION} \
+    "${PYTHON_VERSION}" \
     --publish
 
   status=$?
   [ $status -ne 0 ] && exit 1
 
-  let count++
-
+  (( count++ ))
 done
 
-#Report Image build to Slack
-SLACK_DATA=$(jq -n \
---arg dt "\`Image=${APP_NAME}/${PROC}:${GIT_COMMIT}, ${GIT_TAG}, branch=${GIT_BRANCH}\`" \
-'{"text":$dt}')
-curl -X POST -H 'Content-type: application/json' --data "${SLACK_DATA}" https://hooks.slack.com/services/$SLACK_WORKSPACE_ID/$SLACK_CHANNEL_ID/$SLACK_TOKEN
+# Report image build to Slack
+SLACK_DATA=$(jq -n --arg dt "\`Image=${APP_NAME}/${PROC}:${GIT_COMMIT}, ${GIT_TAG}, branch=${GIT_BRANCH}\`" '{"text":$dt}')
+SLACK_WEBHOOK="https://hooks.slack.com/services/$SLACK_WORKSPACE_ID/$SLACK_CHANNEL_ID/$SLACK_TOKEN"
+curl -X POST -H 'Content-type: application/json' --data "${SLACK_DATA}" "$SLACK_WEBHOOK"
