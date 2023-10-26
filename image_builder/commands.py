3432fcabffd330ba758d20960874966d0e894944
Lawrence Goldstien <lawrence.goldstein@digital.trade.gov.uk>
2023-10-26 08:54:15 +0100

    Notify as soon as command starts

--- image_builder/commands.py
+++ image_builder/commands.py
@@ -10,51 +10,59 @@ from image_builder.progress import Progress
 @click.command("build", help="Build an image")
 @click.option("--publish", is_flag=True, default=False, help="Publish the built image")
 def build(publish):
-    if not Docker.running():
-        click.echo("Docker is not running, starting up...")
-        Docker.start()
-
-    click.echo("Docker is running, continuing with build...")
-
     codebase = Codebase(".")
     notify = Notify(codebase)
     progress = Progress()
     pack = Pack(codebase)
-
-    click.echo(
-        "Found revision: "
-        f"repository={codebase.revision.get_repository_name()}, "
-        f"commit={codebase.revision.commit}, "
-        f"branch={codebase.revision.branch}, "
-        f"tag={codebase.revision.tag}"
-    )
-
-    click.echo(f"Found processes: {[p.name for p in codebase.processes]}")
-    click.echo(f"Found languages: {codebase.languages}")
-    click.echo(
-        f"Using builder: {codebase.build.builder.name}@{codebase.build.builder.version}"
-    )
-    click.echo(f"Using buildpacks: {pack.get_buildpacks()}")
-
     progress.current_phase_running()
     notify.post_progress(progress)
-    buildpacks = ", ".join(pack.get_buildpacks())
-    processes = ", ".join(p.name for p in pack.codebase.processes)
-    notify.post_job_comment(
-        [
-            f"*Repository*: {pack.codebase.revision.get_repository_name()}",
-            f"*Commit*: {pack.codebase.revision.commit} "
-            f"*Branch*: {pack.codebase.revision.branch} "
-            f"*Tag*: {pack.codebase.revision.tag}",
-            f"*Processes*: {processes}",
-            f"*Languages*: {pack.codebase.languages}",
-            f"*Builder*: {pack.codebase.build.builder.name}@{pack.codebase.build.builder.version}",
-            f"*Buildpacks*: {buildpacks}",
-        ]
-    )
 
     try:
+        if not Docker.running():
+            click.echo("Docker is not running, starting up...")
+            Docker.start()
+
+        click.echo("Docker is running, continuing with build...")
+
+        click.echo(
+            "Found revision: "
+            f"repository={codebase.revision.get_repository_name()}, "
+            f"commit={codebase.revision.commit}, "
+            f"branch={codebase.revision.branch}, "
+            f"tag={codebase.revision.tag}"
+        )
+
+        click.echo(f"Found processes: {[p.name for p in codebase.processes]}")
+        click.echo(f"Found languages: {codebase.languages}")
+        click.echo(
+            f"Using builder: {codebase.build.builder.name}@{codebase.build.builder.version}"
+        )
+        click.echo(f"Using buildpacks: {pack.get_buildpacks()}")
+
+        buildpacks = ", ".join(pack.get_buildpacks())
+        processes = ", ".join(p.name for p in pack.codebase.processes)
+        notify.post_job_comment(
+            [
+                f"*Repository*: {pack.codebase.revision.get_repository_name()}",
+                f"*Commit*: {pack.codebase.revision.commit} "
+                f"*Branch*: {pack.codebase.revision.branch} "
+                f"*Tag*: {pack.codebase.revision.tag}",
+                f"*Processes*: {processes}",
+                f"*Languages*: {pack.codebase.languages}",
+                f"*Builder*: {pack.codebase.build.builder.name}@{pack.codebase.build.builder.version}",
+                f"*Buildpacks*: {buildpacks}",
+            ]
+        )
+
         pack.codebase.setup()
+
+        pack.build(
+            publish, on_building(notify, progress), on_publishing(notify, progress)
+        )
+
+        progress.current_phase_success()
+        notify.post_progress(progress)
+
     except (Exception, KeyboardInterrupt) as e:
         progress.current_phase_failure()
         notify.post_progress(progress)
@@ -62,21 +70,6 @@ def build(publish):
             [f"Build was cancelled: {e.__class__.__name__}", str(e)]
         )
         exit(1)
-
-    try:
-        try:
-            pack.build(
-                publish, on_building(notify, progress), on_publishing(notify, progress)
-            )
-            progress.current_phase_success()
-            notify.post_progress(progress)
-        except (Exception, KeyboardInterrupt) as e:
-            progress.current_phase_failure()
-            notify.post_progress(progress)
-            notify.post_job_comment(
-                [f"Build was cancelled: {e.__class__.__name__}", str(e)]
-            )
-            exit(1)
 
     finally:
         codebase.teardown()
