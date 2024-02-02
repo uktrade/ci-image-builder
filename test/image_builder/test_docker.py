7f371e5906f07e1c7a157865aa82dbe660e9bcb3
Samuele Mattiuzzo <1381563+samuele-mattiuzzo@users.noreply.github.com>
2024-02-02 15:41:54 +0000

    Tests updated

--- test/image_builder/test_docker.py
+++ test/image_builder/test_docker.py
@@ -87,13 +87,14 @@ class TestDocker(unittest.TestCase):
         sleep.assert_has_calls([call(1)] * 60)
 
     @patch("subprocess.Popen", return_value=None)
-    @patch("subprocess.run", return_value=StubbedProcess(returncode=0))
+    @patch("subprocess.run", return_value=StubbedProcess(returncodes=[1, 1, 1, 1, 0]))
     @patch("time.sleep", return_value=None)
     def test_starting_docker_with_public_repository(self, sleep, run, popen):
         Docker.start(repository="public.ecr.aws/image-name")
 
         run.assert_has_calls(
             [
+                call("docker ps", stdout=subprocess.PIPE, shell=True),
                 call(
                     "aws ecr get-login-password --region region | docker login --username AWS --password-stdin public.ecr.aws",
                     stdout=subprocess.PIPE,
