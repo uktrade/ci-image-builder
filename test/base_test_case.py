a758d20ac10d72f5d30c8105ab244d0e7dfcba89
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-02-06 12:13:10 +0000

    Extract reset_environment_variables()

--- test/base_test_case.py
+++ test/base_test_case.py
@@ -7,5 +7,8 @@ class BaseTestCase(TestCase):
     def setUp(self):
         super().setUp()
 
+        self.reset_environment_variables()
+
+    def reset_environment_variables(self):
         os.environ.pop("CODEBUILD_BUILD_ARN", None)
         os.environ.pop("ECR_REPOSITORY", None)
